675ed8b79ab77264d3bae99e0ba55b7644d69e22
Mathieson Sterling <msterling@nuvalence.io>
2022-02-24 13:07:57 -0500

    Working with action!

---
+++ dmv/models/type_action.sql
@@ -0,0 +1,15 @@
+SELECT 
+    CAST(id as INT) as id,
+    CAST(user_id as INT) as user_id,
+    CAST(registration_id as INT) as registration_id,
+    CAST(permit_id as INT) as permit_id,
+    CAST(sales_tax_id as INT) as sales_tax_id,
+    CAST(non_driver_id_id as INT) as non_driver_id_id,
+    CAST(license_amendment_id as INT) as license_amendment_id,
+    CAST(license_reciprocity_id as INT) as license_reciprocity_id,
+    CAST(id_upgrade_id as INT) as id_upgrade_id,
+    _airbyte_emitted_at as ingested_at,
+    action_details,
+    office,
+    created_at
+FROM `playground-m-sterling`.airbyte.action
